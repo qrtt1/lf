38af27bae1a60e10b8f66dd849454e572a9b6120
Ching Yi, Chan <qrtt1@infuseai.io>
2024-02-27 16:39:50 +0800

    init: put data and dataset
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ make_dataset.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from datasets import Dataset
+
+
+def main():
+    df = pd.read_excel('kautian.ods', engine='odf', sheet_name='例句')
+    df = df.loc[:, ['漢字', '羅馬字', '華語']]
+
+    dataset = Dataset.from_pandas(df)
+    train_test_split = dataset.train_test_split(test_size=0.2)
+
+    train_dataset = train_test_split['train']
+    train_dataset.save_to_disk('dataset/train')
+
+    test_dataset = train_test_split['test']
+    test_dataset.save_to_disk('dataset/test')
+
+
+if __name__ == '__main__':
+    main()
